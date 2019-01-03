2ac5f44cd53b6f4881395d58e1293a281c289db9
Chris Schappert <cn.schappert@gmail.com>
2019-01-04 00:24:19 +0900

    edit solution for Day 01 Part 02 (it works now and is pretty fast)

--- app/Day01P2.hs
+++ app/Day01P2.hs
@@ -6,6 +6,9 @@ import qualified Data.ByteString as ByteString
 import Data.Text (Text)
 import qualified Data.Text as Text
 
+import Data.Set (Set)
+import qualified Data.Set as Set
+
 -- You can import from shared modules defined in src/.
 import Lib (someFunc)
 
@@ -14,21 +17,35 @@ main =
   interact mySolution
 
 -- | This is a function that has been passed to interact above.
+--
+-- Tests:
+-- >>> mySolution "+1\n+1\n-3"
+-- "0"
+-- >>> mySolution "+1\n-1\n"
+-- "0"
+-- >>> mySolution "+3\n+3\n+4\n-2\n-4\n"
+-- "10"
+-- >>> mySolution "-6\n+3\n+8\n+5\n-6\n"
+-- "5"
+--
 mySolution :: String -> String
-mySolution input = show $ findDuplicateFreq ls [0]
-    where ls = map parseSignedInt $ lines $ input
+mySolution input = show $ findDuplicatedFreq ls
+    where ls = cycle $ map parseSignedInt $ lines $ input
 
-findDuplicateFreq :: (Num a, Eq a) => [a] -> [a] -> a
-findDuplicateFreq [] _ = 666
-findDuplicateFreq xs [] = findDuplicateFreq xs [0]
-findDuplicateFreq (x:xs) ys
-  | elem (x + (last ys)) ys = x + (last ys)
-  | otherwise = findDuplicateFreq (xs ++ [x]) (ys ++ [x + last ys])
+findDuplicatedFreq :: (Ord a, Num a, Eq a) => [a] -> a
+findDuplicatedFreq xs = findDuplicatedFreq' xs 0 (Set.fromList [0])
+
+findDuplicatedFreq' :: (Ord a, Num a, Eq a) => [a] -> a -> Set a -> a
+findDuplicatedFreq' [] _ _ = 666
+findDuplicatedFreq' (x:xs) acc pastFreqs
+  | Set.member nextFreq pastFreqs = nextFreq
+  | otherwise = findDuplicatedFreq' xs nextFreq (Set.insert nextFreq pastFreqs)
+  where nextFreq = acc + x
 
 parseSignedInt :: String -> Int
 parseSignedInt [] = 0
-parseSignedInt ('-':xs) = negate (read xs)
-parseSignedInt (_:xs) = read xs
+parseSignedInt ('+':xs) = read xs
+parseSignedInt xs = read xs
 
 -- | This is an example of using doctest.  This is easy to use to test
 -- functions you are working on.  It is much lighter weight than using
